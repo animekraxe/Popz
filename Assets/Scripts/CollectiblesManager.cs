361c3bbc09f641308ab8b17a59742524641f9b72
Samantha Chen <sc2337@cornell.edu>
2014-06-27 12:17:59 -0700

    started working more on collectibles

---
+++ Assets/Scripts/CollectiblesManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CollectiblesManager : MonoBehaviour {
+
+	public Queue<Collectible> collectibles;
+
+	// Use this for initialization
+	void Start () {
+		collectibles = new Queue<Collectible> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
